e8df997e3382f1d01121fbd704c9c51372d5616a
Tyson Fernstrom <tyson.fernstrom@gmail.com>
2023-10-28 11:06:35 -0500

    fix branch errors

--- js/app.js
+++ js/app.js
@@ -86,15 +86,11 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
   if (!sumArr || !Array.isArray(sumArr) || sumArr.length === 0) {
-    // Handle the case where sumArr is undefined, not an array, or an empty array.
     return [0, 'No valid input provided.'];
   }
-  // Calculate the sum of the numbers in the array using the `sum` function you've created.
   const sumResult = sumArr.reduce((accumulator, currentValue) => {
     return sum(accumulator, currentValue)[0];
   }, 0);
-
-  // Create the formatted string using string concatenation.
   const formattedString = sumArr.join(',') + ' was passed in as an array of numbers, and ' + sumResult + ' is their sum.';
 
   return [sumResult, formattedString];
@@ -118,8 +114,47 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  if (!multArr || !Array.isArray(multArr) || multArr.length === 0) {
+    // Handle the case where multArr is undefined, not an array, or an empty array.
+    return [0, 'No valid input provided.'];
+  }
+
+  const product = multArr.reduce((accumulator, currentValue) => {
+    if (typeof accumulator === 'number' && typeof currentValue === 'number') {
+      return accumulator * currentValue;
+    } else {
+      // Handle cases where elements in the array are not numbers.
+      return accumulator;
+    }
+  }, 1);
+
+  const formattedString = 'The numbers ' + multArr.join(',') + ' have a product of ' + product + '.';
+  return [product, formattedString];
+}
+
+/* Problem 4
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+
+"2,3,4 was passed in as an array of numbers, and 9 is their sum."
+
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+
+// Write your code here
+let testArray = [2, 3, 4]; //eslint-disable-line
+
+function sumArray(sumArr) { //eslint-disable-line
 
 }
+
+// Here is the test for sumArray(); uncomment it to run it
+
+testSumArray(testArray);
+
+
+function sumArray(sumArr) {
+  
 
 // Here is the test for multiplyArray(); uncomment it to run it
 //testMultiplyArray(testArray);
