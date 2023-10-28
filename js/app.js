e9120a3d0225a60e94a3d0d84728d0c5418e061e
Tyson Fernstrom <tyson.fernstrom@gmail.com>
2023-10-28 11:19:13 -0500

    solved problem five

--- js/app.js
+++ js/app.js
@@ -115,7 +115,6 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
   if (!multArr || !Array.isArray(multArr) || multArr.length === 0) {
-    // Handle the case where multArr is undefined, not an array, or an empty array.
     return [0, 'No valid input provided.'];
   }
 
@@ -123,7 +122,6 @@ function multiplyArray(multArr) { //eslint-disable-line
     if (typeof accumulator === 'number' && typeof currentValue === 'number') {
       return accumulator * currentValue;
     } else {
-      // Handle cases where elements in the array are not numbers.
       return accumulator;
     }
   }, 1);
@@ -131,35 +129,10 @@ function multiplyArray(multArr) { //eslint-disable-line
   const formattedString = 'The numbers ' + multArr.join(',') + ' have a product of ' + product + '.';
   return [product, formattedString];
 }
-
-/* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
-"2,3,4 was passed in as an array of numbers, and 9 is their sum."
-
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
-
-// Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
-
-function sumArray(sumArr) { //eslint-disable-line
-
-}
-
-// Here is the test for sumArray(); uncomment it to run it
-
-testSumArray(testArray);
-
-
-function sumArray(sumArr) {
-  
-
 // Here is the test for multiplyArray(); uncomment it to run it
-//testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
